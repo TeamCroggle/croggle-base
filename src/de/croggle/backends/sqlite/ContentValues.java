0f56b7a90e7218cdb8ae4efc3da49dbac392f8ab
Lukas Boehm <suluke93@gmail.com>
2014-02-19 10:55:47 +0100

    Set up eclipse project in croggle-base so changes can be made with the ide's support

--- src/de/croggle/backends/sqlite/ContentValues.java
+++ src/de/croggle/backends/sqlite/ContentValues.java
@@ -9,5 +9,17 @@ public interface ContentValues {
 
 	void put(String key, boolean value);
 
+	/**
+	 * Returns the object backing this {@link ContentValues}. Actually, this
+	 * method should rather be package private, as only implementations of the
+	 * concrete backends need it. But this is obviously not possible. Don't use
+	 * it though!
+	 * 
+	 * Usage example: The concrete Android {@link Database} implementation uses
+	 * it to get the Android's ContentValues out of croggle's ContentValues to
+	 * apply it with the help of Android's SQLiteOpenHelper.
+	 * 
+	 * @return the object backing this {@link ContentValues}
+	 */
 	Object get();
 }
