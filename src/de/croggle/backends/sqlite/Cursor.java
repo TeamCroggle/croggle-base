3c4684a0e8fa9a905cb8744cf11655e29aed816c
Lukas Boehm <suluke93@gmail.com>
2014-02-19 11:05:58 +0100

    Remove ContentValues.getObject, for I had a better idea

--- src/de/croggle/backends/sqlite/Cursor.java
+++ src/de/croggle/backends/sqlite/Cursor.java
@@ -3,13 +3,13 @@ package de.croggle.backends.sqlite;
 public interface Cursor {
 	boolean moveToFirst();
 
+	boolean moveToNext();
+
 	int getColumnIndex(String name);
 
 	String getString(int index);
 
 	int getInt(int columnIndex);
-
-	boolean moveToNext();
 
 	float getFloat(int column);
 }
